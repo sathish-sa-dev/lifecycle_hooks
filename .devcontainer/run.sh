4e007f3c949f377070e97905bce5d949fdb6aadb
Josh Spicer <josh@joshspicer.com>
2022-11-17 19:49:07 +0000

    add repo scoped secret

--- .devcontainer/run.sh
+++ .devcontainer/run.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 LIFECYCLE_HOOK="$1"
+
+# Only works on lifecycle hooks run in the container ('initializeCommand' won't be able to find this.)
 ACTION_NAME="$(cat /workspaces/.codespaces/shared/environment-variables.json | jq -r .ACTION_NAME)"
 
-CONTENTS="`date +%y-%m-%d_%T.%N`-$LIFECYCLE_HOOK-$ACTION_NAME.marker"
-echo $CONTENTS
-touch $CONTENTS
+MARKER_FILE="`date +%y-%m-%d_%T.%N`-$LIFECYCLE_HOOK-$ACTION_NAME.marker"
+echo $MARKER_FILE
+touch $MARKER_FILE
+
+echo "MY_REPO_SCOPED_SECRET=$MY_REPO_SCOPED_SECRET" >> $MARKER_FILE
 
