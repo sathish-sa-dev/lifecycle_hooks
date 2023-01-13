0da576f491529f3fe8b5b2280354114cc8b3b773
Josh Spicer <josh@joshspicer.com>
2023-01-13 12:59:58 -0800

    Update run.sh

--- .devcontainer/run.sh
+++ .devcontainer/run.sh
@@ -2,6 +2,8 @@
 
 LIFECYCLE_HOOK="$1"
 
+echo "EXECUTING HOOK: $LIFECYCLE_HOOK"
+
 # Only works on lifecycle hooks run in the container ('initializeCommand' won't be able to find this.)
 ACTION_NAME="$(cat /workspaces/.codespaces/shared/environment-variables.json | jq -r .ACTION_NAME)"
 
