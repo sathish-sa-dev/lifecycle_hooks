a0531d38cc48c8d0429ee40018833bab4d9b4c4d
Josh Spicer <josh@joshspicer.com>
2023-07-12 16:50:46 +0000

    print effective user ID

--- .devcontainer/run.sh
+++ .devcontainer/run.sh
@@ -2,7 +2,12 @@
 
 LIFECYCLE_HOOK="$1"
 
-echo "EXECUTING HOOK: $LIFECYCLE_HOOK"
+echo "----- EXECUTING HOOK: '$LIFECYCLE_HOOK' -----"
+
+# Print out the user id and name running this script
+USER_ID=$(id -u)
+USER_NAME=$(id -un)
+echo "Executing as user $USER_NAME (id: $USER_ID)"
 
 # Only works on lifecycle hooks run in the container ('initializeCommand' won't be able to find this.)
 ACTION_NAME="$(cat /workspaces/.codespaces/shared/environment-variables.json | jq -r .ACTION_NAME)"
@@ -19,3 +24,5 @@ echo "MY_USER_SCOPED_SECRET=$MY_USER_SCOPED_SECRET" >> $MARKER_FILE
 
 # Eg: https://github.com/organizations/codspace/settings/secrets/codespaces
 echo "MY_ORG_SCOPED_SECRET=$MY_ORG_SCOPED_SECRET" >> $MARKER_FILE
+
+echo "----- DONE EXECUTING HOOK '$LIFECYCLE_HOOK' -----"
